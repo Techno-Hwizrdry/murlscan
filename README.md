e0d3468a67ec269f5f551eef6a5ecfdbc054bd20
Alexan Mardigian <alex.mardigian@gmail.com>
2022-02-12 04:21:44 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# MurlScan
+A Python 3 script that detects suspicious URLs.
+
+## Prerequisites
+This module requires python3 (version 3.6 or later) and python3-pip.
+
+These prerequisites can be installed on a Debian based linux machine, like so:
+
+`sudo apt-get install python3 python3-pip`
+
+You will also need an API key from IP Quality Score to use this module.  You can sign up for one [here.](https://www.ipqualityscore.com/create-account)
+
+## Setup
+Once those prerequisites have been installed, git clone this repo, cd into it, and set up the virtual environment:
+
+`cd /path/to/murlscan && ./setup_virtualenv.sh`
+
+setup_virtualenv.sh will set murlscan as the virtual environment, activate it, and call pip3 to download and install all the python3 dependencies for this script.  These dependencies are listed in requirements.txt.
+
+### MurlScan Config File
+Set APIKEY in murlscan.conf to your IP Quality Score API key.
+STRICTNESS can be either 0, 1, or 2.  Stricter checks may provide a higher false-positive rate. It is recommend to defaulting to level "0", the lowest strictness setting, and increasing to "1" or "2" depending on your levels of abuse.
+
+## Usage
+To scan a single URL:
+`python3 murlscan.py -u https://example.com`
+
+To scan multiple URLs:
+`python3 murlscan.py -i urls.txt`
+
+Your input text file should list 1 URL per line.
+
+To output the results with all their fields to a csv file:
+`python3 murlscan.py -i urls.txt -o output.csv`
+
+murlscan.py will use ./murlscan.conf by default, but you can specify a different filepath if it's located elsewhere:
+`python3 murlscan.py -i urls.txt -c path/to/murlscan.conf`
+
+Help:
+`python murlscan.py -h`
---
+++ murlscan.py
@@ -0,0 +1,99 @@
+# Copyright: 2022, Alexan Mardigian
+__version__ = "1.0.0"
+
+import sys
+from argparse     import ArgumentParser
+from configparser import ConfigParser
+from csv import DictWriter
+from os.path import exists
+from malurl  import MalURL
+
+CONFIG_FILE = 'murlscan.conf'
+
+def get_args():
+    parser = ArgumentParser()
+    parser.add_argument('-c', dest='conf_filepath',   help='File path to MurlScan configuration file.',
+                                                      default=CONFIG_FILE)
+    parser.add_argument('-u', dest='url',             help='URL of suspicious link.',
+                                                      required=False)
+    parser.add_argument('-i', dest='input_filepath',  help='File path to a file listing URLs.',
+                                                      required=False)
+    parser.add_argument('-o', dest='output_filepath', help='File path for the output.',
+                                                      default='')
+
+    if len(sys.argv) == 1:
+        parser.print_help()
+        sys.exit(1)
+
+    return parser.parse_args()
+
+def print_and_exit(msg):
+    print(msg)
+    sys.exit(0)
+
+def read_config_file(filename):
+    config = ConfigParser()
+    config.read(filename)
+    return config
+
+def read_urls(filename):
+    if not exists(filename): return []
+
+    with open(filename, 'r') as infile:
+        return [line.strip() for line in infile.readlines()]
+
+def get_data(config):
+    cfg = config['murlscan']
+    apikey = cfg['apikey']
+    strictness = cfg['strictness']
+    return MalURL(apikey, strictness)
+
+def print_results(murl, urls):
+    for url in urls:
+        murl.fetch(url)
+        murl.print()
+        print('\n')
+
+def write_csv(murl, urls, filename):
+    with open(filename, 'w') as csvfile:
+        count = 0
+        for url in urls:
+            count += 1
+            murl.fetch(url)
+            field_names = murl.results.keys()
+            writer = DictWriter(csvfile, fieldnames=field_names)
+            if count == 1: writer.writeheader()
+            writer.writerow(murl.results)
+
+def main():
+    opts = get_args()
+    infile = opts.input_filepath
+    
+    if not infile and not opts.url:
+        msg = "Error: No URL or list of URLs provided.\nFor help, run:  'python3 murlscan.py --help'"
+        print_and_exit(msg)
+
+    config_file = opts.conf_filepath
+
+    try:
+        config = read_config_file(config_file)
+        urls = [opts.url] if opts.url else read_urls(infile)
+
+        if not urls:
+            print_and_exit("Error: file {infile} not found.")
+
+        murl = get_data(config)
+
+        if opts.url:
+            print_results(murl, urls)
+        else:
+            print(f'Writing to {opts.output_filepath} ...')
+            write_csv(murl, urls, opts.output_filepath)
+            print("Done!")
+    except KeyError as e:
+        print(f"Error: {e.args[0]} section missing from config file '{config_file}'.")
+    except FileNotFoundError as e:
+        print(e)
+
+if __name__ == '__main__':
+    main()
